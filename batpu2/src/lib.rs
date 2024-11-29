d42b89bdc32ab4a77f971db769b2c8b0650eca36
sebimoe <sebi@sebi.moe>
2024-11-29 06:07:14 +0000

    Hello word test

--- batpu2/src/lib.rs
+++ batpu2/src/lib.rs
@@ -20,4 +20,17 @@ mod tests {
     fn it_works() {
         BatPU2::new([0, 0, 1, 2, 3]);
     }
+
+    #[test]
+    #[cfg(feature = "embedded_io")]
+    fn hello_world() {
+        let code: Vec<u16> = vec![
+            0x8ff9, 0xff00, 0x8ff7, 0x8e08, 0xffe0, 0x8e05, 0xffe0, 0x8e0c, 0xffe0, 0x8e0c, 0xffe0, 0x8e0f, 0xffe0,
+            0x8e17, 0xffe0, 0x8e0f, 0xffe0, 0x8e12, 0xffe0, 0x8e0c, 0xffe0, 0x8e04, 0xffe0, 0x8ff8, 0xff00, 0x1000
+        ];
+
+        let mut vm = BatPU2::new(code);
+        assert_eq!(vm.step_multiple(30), 26);
+        assert_eq!(vm.io.char_display.to_string(), String::from("HELLOWORLD"));
+    }
 }
